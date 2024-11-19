5db07b90931ba64f50bddfbb02d23396b3f17ed9
Datata1 <jan-david.wiederstein@codesphere.com>
2024-11-19 18:53:54 +0100

    code changes

--- src/custom_types/db_models.py
+++ src/custom_types/db_models.py
@@ -3,9 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 # Basis-Klasse für ORM-Modelle
-Base = declarative_base()
-
-
+Base = declarative_base()    
+    
 class Order(Base):
     __tablename__ = 'orders'
 
